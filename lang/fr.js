62ad776f7bffee7910999b7d268b910d05e23cee
Seth Clydesdale <sethc1995@gmail.com>
2017-06-30 14:01:36 -0400

    create lang folder for imports

---
+++ lang/fr.js
@@ -0,0 +1,47 @@
+// by DDril
+window.fam_lang_import = {
+  loading : 'Chargement...',
+  idle : 'Forumactif Messenger est en veille suite à votre inactivité. Cliquez ici pour le relancer.',
+
+  new_topic : 'Nouveau sujet',
+  start_topic : 'Débuter un sujet',
+
+  msg_placeholder : 'Entrer un message',
+  title_placeholder : 'Titre du sujet',
+
+  no_tabs_title : 'Ouvrir un nouvel onglet',
+  no_tabs : 'Oh ! Vous ne possédez aucun onglet, cliquez ici pour en ajouter un !',
+
+  attach_image : 'Ajouter une image',
+  attach_gif : 'Ajouter un gif',
+  attach_video : 'Ajouter une vidéo',
+  attach_remove : 'Gérer ses fichiers attachés',
+  attach_upload : 'Téléverser',
+  attach_uploading : 'Le téléversement d\'image est toujours ouvert.',
+  attach_searchYT : 'Recherche YouTube',
+  attach_searchingYT : 'YouTube est déjà ouvert.',
+  attach_max : 'Vous avez atteint le nombre de fichiers joints sur ce message.',
+  attach_success : 'Fichier ajouté !',
+  attach_none : 'Vous avez aucun fichier.',
+
+  giphy_search : 'Recherche d\'un GIF...',
+  giphy_searching : 'Recherche...',
+  giphy_not_found : 'Aucun résultat.',
+
+  tooltip_openFAM : 'Forumactif Messenger',
+  tooltip_back : 'Cliquez pour revenir en arrière',
+  tooltip_menu : 'Ouvrir le menu',
+  tooltip_home : 'Accueil',
+  tooltip_settings : 'Paramètres',
+  tooltip_about : 'A propos',
+  tooltip_attachments : 'Ajout d\'un fichier',
+  tooltip_emoji : 'Ajout d\'un smiley',
+  tooltip_send : 'Envoyer un message',
+
+  error_sending : 'Un soucis a été rencontré à l\'envoi du message. Voulez-vous le renvoyer ?',
+  error_resend : 'Renvoyer',
+  error_delete : 'Supprimer',
+  error_report : 'Signaler',
+
+  guest : 'Invité'
+};
